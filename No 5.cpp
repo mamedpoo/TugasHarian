e568ad3efc893495f5cbdf1b254b42f0e0435311
mamed_poo <barapoi5@gmail.com>
2017-11-08 22:09:50 +0800

    menghitung dan membuat perhitungan berat badan ideal

--- No 5.cpp
+++ No 5.cpp
@@ -3,4 +3,25 @@
 using namespace std;
 
 int main(){
-}
+		double berat, tinggi;
+	
+	cout <<"Perhitungan Berat Badan Ideal"<<endl;
+	cout <<"Masukkan Berat Badan Anda : ";
+	cin >>berat;
+	cout <<"Masukkan Tinggi Badan Anda : ";
+	cin >>tinggi;
+	
+	cout <<"Hasil Perhitungan Anda"<<endl;
+	cout<<"Berat Badan Anda : "<<berat<<endl;
+	cout<<"Tinggi Badan Anda : "<<tinggi<<endl;
+	if(berat < tinggi/2.5){
+		cout <<"Anda Underweight";
+	}
+	else if(tinggi/2.3 < berat){
+		cout <<"Anda Overweight";
+	}
+	else if(tinggi/2.5 <= berat <= tinggi/2.3){
+		cout <<"Anda Normal";
+	}
+
+}	
