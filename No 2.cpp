742ef27e1ae4642d877a7c7cda3b60d4afd1d9ff
mamed_poo <barapoi5@gmail.com>
2017-11-08 22:03:01 +0800

    menghitung dan menampilkan Perhitungan persegi panjang dan segitiga

--- No 2.cpp
+++ No 2.cpp
@@ -2,6 +2,35 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char** argv){
+	int pjg,lbr,l,pil,a,t;
 	
-}
+	cout <<"1. Persegi Panjang\n";
+	cout <<"2. Segitiga\n";
+	cout <<"Masukkan Pilihan : ";
+	cin >>pil;
+	
+	switch(pil){
+			case 1:
+			cout<<"Masukkan Panjang = ";
+			cin >>pjg;
+			cout<<"Masukkan Lebar = ";
+			cin >>lbr;
+			l=pjg * lbr;
+			cout<<"Luas Persegi Panjang = "<<l<<"\n";
+			system ("Pause");
+			break;
+			case 2:
+			cout<<"Masukkan Alas Segitiga = ";
+			cin>>a;
+			cout<<"Masukkan Tinggi Segitiga = ";
+			cin>>t;
+			l=0.5*a*t;
+			cout<<"Luas Segitiga Adalah : "<<l<<"\n";
+			system("Pause");
+			 
+	}
+	
+}	
+
+
