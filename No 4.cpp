2344534cc12d8526771621595958b29d04375103
mamed_poo <barapoi5@gmail.com>
2017-11-08 22:08:12 +0800

    menghitung dan menampilkan menentukan nilai positif, negatif, nol

--- No 4.cpp
+++ No 4.cpp
@@ -3,4 +3,18 @@
 using namespace std;
 
 int main (){
+		int x;
+	cout << "Masukkan Nilai : ";
+	cin >>x;
+	
+	if (x>0){
+		cout <<"Nilai Positif";
+	}
+	else if (x<0){
+		cout <<"Nilai Negatif";
+	}
+	else{
+		cout <<"Nilai Nol";
+	}
+
 }
